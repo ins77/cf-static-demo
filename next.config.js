68e456d184fc1a7e426138f24e323be38f3d345f
Дмитрий Сухинин <home@MacBook-Pro-Dmitrij-2.local>
2022-12-29 14:52:31 +0300

    new

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  experimental: {
+    externalDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
