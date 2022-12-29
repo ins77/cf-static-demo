6ca64bd8d4e532a81e352c4a1948afa7075fc04d
Дмитрий Сухинин <home@MacBook-Pro-Dmitrij-2.local>
2022-12-29 16:31:14 +0300

    new

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
 
-  env-cmd -f ./.env.development yarn next build && yarn next export
+  env-cmd -f ./envprod.env yarn next build && yarn next export
 
 else
 
-  env-cmd -f ./.env.production yarn next build && yarn next export
+  env-cmd -f ./envdev.env yarn next build && yarn next export
 
 fi
