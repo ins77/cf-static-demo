974e5bc314d6a29522946db9255a0c2509208d96
Дмитрий Сухинин <home@MacBook-Pro-Dmitrij-2.local>
2022-12-29 16:02:11 +0300

    new

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 if [ "$CF_PAGES_BRANCH" == "production" ]; then
 
-  cross-env APP_ENV=production yarn next build && yarn next export
+  cross-env NEXT_PUBLIC_APP_ENV=production yarn next build && yarn next export
 
 else
 
-  cross-env APP_ENV=development yarn next build && yarn next export
+  cross-env NEXT_PUBLIC_APP_ENV=development yarn next build && yarn next export
 
 fi
