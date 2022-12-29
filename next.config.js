6eb70e38666b84b6646a4c012b2d75f8317849bd
Дмитрий Сухинин <home@MacBook-Pro-Dmitrij-2.local>
2022-12-29 14:43:25 +0300

    new

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: "experimental-edge",
-    externalDir: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    unoptimized: true,
-    loader: "akamai",
-    path: "",
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
