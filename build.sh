480b547fcd6f1e736de87203dba5fa54317ef2ca
Дмитрий Сухинин <home@MacBook-Pro-Dmitrij-2.local>
2023-01-10 12:56:38 +0300

    222

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
-if [ "$CF_PAGES_BRANCH" == "main" ]; then
+# !/bin/bash
 
+# cloudflare build command: bash build.sh
+
+chain="${CHAIN:-testnet}"
+
+if [ chain === "mainnet" ]; then
   yarn build_mainnet && yarn next export
-
 else
-
   yarn build_testnet && yarn next export
-
 fi
